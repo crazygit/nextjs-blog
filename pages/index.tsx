bb0bdcf9fa4807589f0aab57ba8c377580b1a5f1
Crazygit <lianglin999@gmail.com>
2022-04-12 14:50:27 +0800

    fix import error

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { GetStaticProps } from "next/types";
 
 // export async function getStaticProps() {
 //   const allPostsData = getSortedPostsData()
